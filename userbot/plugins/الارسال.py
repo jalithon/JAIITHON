06b9ad4ea6ee8591a25432c1f09274809918a63b
JAIITHON-BR <96321569+JAIITHON-BR@users.noreply.github.com>
2021-12-25 17:48:58 +0300

    Update الارسال.py

--- userbot/plugins/الارسال.py
+++ userbot/plugins/الارسال.py
@@ -1,9 +1,5 @@
-#  قبل لا تفكر تخمط هذا الملف ترا الملف متعوب عليه لا تخمط واني حذرتك
-# حسب قوانين موقع github https://github.com/JMTHON-AR/JM-THON/blob/master/LICENSE
-# تنص على انه اي شخص ياخذ الملف بدون ذكر حقوق طبع والنشر سيتم حذف حسابه من قبل صاحب الملف اقتضى التنوي
-# Copyright ©️ 2021 RR9R7 . All Rights Reserved
-# You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits -  (  @RR7PP  - @JMTHON  )
+
+# ##Credits -  (  @XRZOC  - @JAIITHON  )
 #
 from telethon.tl.types import Channel, MessageMediaWebPage
 from userbot.utils import admin_cmd
