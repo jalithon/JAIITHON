e774388ace2c339ea74eaa7eda71703f675cf8d7
JAIITHON-BR <96321569+JAIITHON-BR@users.noreply.github.com>
2021-12-25 22:50:37 +0300

    Update المطور.py

--- userbot/plugins/المطور.py
+++ userbot/plugins/المطور.py
@@ -39,7 +39,8 @@ async def amireallyalive(event):
         PIC = random.choice(A_IMG)
         cat_caption = f"𝘑𝘈𝘐𝘐𝘛𝘏𝘖𝘕 𝘋𝘌𝘝𝘌𝘓𝘖𝘗𝘗𝘌𝘙\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        cat_caption += f"- Evan  :   @xrzoc\n"
+        cat_caption += f"- EVAN :  @XRZOC\n"
+        cat_caption += f"- CH :   @JAIITHON\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
